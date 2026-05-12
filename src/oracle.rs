7429b5f2e0749682b963b37a6c6a0c680b471dad
virex <182297269+hnpf@users.noreply.github.com>
2026-05-12 17:25:58 -0500

    Add fetch function to retrieve command output

--- src/oracle.rs
+++ src/oracle.rs
@@ -1,1 +1,9 @@
-
+pub fn fetch(cmd: &str) -> Result<String, reqwest::Error> {
+    let url = format!("https://cheat.sh/{}", cmd);
+    let client = reqwest::blocking::Client::new();
+    let res = client.get(url)
+        .header("User-Agent", "curl") // cheat.sh returns ansi-colored text for curl UnU
+        .send()?
+        .text()?;
+    Ok(res)
+}
