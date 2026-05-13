15da8151900d8b4ad5dd64b92ed39a54d40201fe
virex <182297269+hnpf@users.noreply.github.com>
2026-05-12 20:27:05 -0500

    Rename fetch function to get

--- src/ask.rs
+++ src/ask.rs
@@ -1,8 +1,8 @@
-pub fn fetch(cmd: &str) -> Result<String, reqwest::Error> {
+pub fn get(cmd: &str) -> Result<String, reqwest::Error> {
     let url = format!("https://cheat.sh/{}", cmd);
     let client = reqwest::blocking::Client::new();
     let res = client.get(url)
-        .header("User-Agent", "curl") // cheat.sh returns ansi-colored text for curl UnU
+        .header("User-Agent", "curl") // cheat.sh returns ansi-colored text for curl
         .send()?
         .text()?;
     Ok(res)
